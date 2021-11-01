e4727387f0e364d1d1b6b7a9a97cb0cdc439af67
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-11-01 18:51:59 -0500

    Initial Commit

---
+++ SwiftUI-Navigation/SwiftUI_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavigationApp.swift
+//  SwiftUI-Navigation
+//
+//  Created by R. Mark Volkmann on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
