b832041fad9424aa1ef5a71d82fb2146eec0877d
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-11-01 19:11:22 -0500

    initial version

--- SwiftUI-Navigation/ContentView.swift
+++ SwiftUI-Navigation/ContentView.swift
@@ -1,16 +1,57 @@
-//
-//  ContentView.swift
-//  SwiftUI-Navigation
-//
-//  Created by R. Mark Volkmann on 11/1/21.
-//
-
 import SwiftUI
+
+struct MainPage: View {
+    var body: some View {
+        VStack {
+            Text("This is on the main page.")
+            NavigationLink(destination: ChildPage()) {
+                Text("Go to child page")
+            }
+        }
+    }
+}
+
+struct ChildPage: View {
+    var body: some View {
+        VStack {
+            Text("This is on the child page.")
+            NavigationLink(destination: Grandchild1Page()) {
+                Text("Go to grandchild one page")
+            }
+            NavigationLink(destination: Grandchild2Page()) {
+                Text("Go to grandchild two page")
+            }
+        }
+    }
+}
+
+struct Grandchild1Page: View {
+    var body: some View {
+        Text("This is on the grandchild one page.")
+    }
+}
+
+struct Grandchild2Page: View {
+    var body: some View {
+        Text("This is on the grandchild two page.")
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // Usually want this at top-level.
+        // One exception is when using multiple tabs
+        // where each has its own NavigationView.
+        NavigationView {
+            MainPage()
+            // This goes on a view inside NavigationView,
+            // not on the NavigationView,
+            // because the title can change for each page.
+            // displayMode has three options:
+            // large (default), inline, and automatic.
+            // Automatic uses large for the top view and inline for others.
+            .navigationBarTitle("Main", displayMode: .large)
+        }
     }
 }
 
